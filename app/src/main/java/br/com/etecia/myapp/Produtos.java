6f7fde25de04d18ebdbc8bbe0133704bf45e6586
yazribeiroo <yasmim.rm05@gmail.com>
2025-08-08 20:11:48 -0300

    criação de card

--- app/src/main/java/br/com/etecia/myapp/Produtos.java
+++ app/src/main/java/br/com/etecia/myapp/Produtos.java
@@ -1,4 +1,33 @@
 package br.com.etecia.myapp;
 
 public class Produtos {
+
+    private String titulo;
+
+    private int imagem;
+
+
+
+
+    public Produtos(String titulo, int imagem) {
+        this.titulo = titulo;
+        this.imagem = imagem;
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
