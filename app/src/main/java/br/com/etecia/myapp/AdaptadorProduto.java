74d1bdc751998412c58708e127f28451bbc41298
yazribeiroo <yasmim.rm05@gmail.com>
2025-08-08 22:12:49 -0300

    crian a view

--- app/src/main/java/br/com/etecia/myapp/AdaptadorProduto.java
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorProduto.java
@@ -1,17 +1,33 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public abstract class AdaptadorProduto extends RecyclerView.Adapter<AdaptadorProduto.ViewHolder>  {
+    private Context nossocontexto;
+    private List<Produtos> nossalistaProdutos;
+
+    public AdaptadorProduto(Context nossocontexto) {
+        this.nossocontexto = nossocontexto;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(nossocontexto);
+        View view = inflater.inflate
+                (R.layout.modelo_produtos,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -21,12 +37,20 @@ public abstract class AdaptadorProduto extends RecyclerView.Adapter<AdaptadorPro
 
     @Override
     public int getItemCount() {
-        return 0;
+        return nossalistaProdutos.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        CardView cardProduto;
+
+        ImageView idmodimagemproduto;
+
+        TextView idtituloproduto;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            cardProduto = itemView.findViewById(R.id.cardProduto);
+            idmodimagemproduto = itemView.findViewById(R.id.idmodimagemproduto);
+            idtituloproduto = itemView.findViewById(R.id.idtituloproduto);
         }
     }
 }
